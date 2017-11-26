8e291369bfcb3855319bafdbd7fe9b596c136ae9
deepakdevaru <joshi.deepak26@gmail.com>
2017-11-26 14:33:05 -0800

    Transport layer protocols implemented

--- Packet-Sniffer.py
+++ Packet-Sniffer.py
@@ -4,8 +4,8 @@ import struct
 import sys
 
 def ethernet_unpack(pkt):
-    dest_mac, src_mac,protocol= struct.unpack('!6s 6s H', pkt[:14])
-    return mac_address(dest_mac), mac_address(src_mac) , socket.htons(protocol), pkt[14:]
+    dest_mac, src_mac,type= struct.unpack('!6s 6s H', pkt[:14])
+    return mac_address(dest_mac), mac_address(src_mac) , socket.htons(type), pkt[14:]
 
 #formating mac to AA:BB:CC format
 def mac_address(address_data):
@@ -22,17 +22,121 @@ def mac_address(address_data):
 #Source IP: 32 bits : hence 4s ( string of size 8*4)
 #Destination IP: 32 bits : hence 4s ( string of size 8*4)
 def IP_header_unpack(pkt):
-    unpack_data= struct.unpack('!B B H H H B B H 4s 4s', pkt[:20])
+    eth_length= 14
+    unpack_data= struct.unpack('!B B H H H B B H 4s 4s', pkt[eth_length:20+eth_length])
     versionIHL= unpack_data[0] #unpack version and  IP header length
 
     version= versionIHL >> 4 # to extract version, bit shift by 4 bits as version occupies 4bits in iptable
-    header_length =  versionIHL & 0xf
+    header_length =  (versionIHL & 0xf)*4
 
     # differentiated services not unpacked
 
     total_length=  unpack_data[2]
     identification= unpack_data[3]
-    return version, header_length, total_length, identification
+    ttl=unpack_data[5]
+    ip_protocol= unpack_data[6]
+
+    protocol_decider(ip_protocol,header_length,eth_length,pkt)
+
+    source_addr= socket.inet_ntoa(unpack_data[8]) #get source ip
+    dest_addr=socket.inet_ntoa(unpack_data[9]) #get destination ip
+    return version, header_length, total_length, identification,ttl,ip_protocol,source_addr,dest_addr
+
+
+
+
+def protocol_decider(proto_number,header_length,eth_length,pkt):
+
+
+    if proto_number== 6: #TCP
+
+         protocol,source_port,dest_port,sequence,acknum = TCP_protocol (header_length,eth_length,pkt)
+         print 'protocol: '+ str(protocol)
+         print 'source port: '+ str(source_port)
+         print 'destination port: ' +str(dest_port)
+         print 'sequence: '+ str(sequence)
+         print 'acknum: '+str(acknum)
+         return
+
+    elif proto_number==1: #ICMP
+        protocol,type, code, checksum =ICMP_packet(header_length, eth_length, pkt)
+        print 'protocol: ' + str(protocol)
+        print 'type: ' + str(type)
+        print 'code: ' + str(code)
+        print 'checksum: ' + str(checksum)
+        return
+
+    elif proto_number==17: #UDP
+        protocol,source_port, dest_port, length, check_sum= UDP_packet(header_length,eth_length,pkt)
+        print 'protocol: ' + str(protocol)
+        print 'source port: ' + str(source_port)
+        print 'destination port: ' + str(dest_port)
+        print 'length: ' + str(length)
+        print 'checksum: ' + str(check_sum)
+        return
+    else:
+        return 'some other protocol'
+
+
+
+
+
+
+
+
+#TCP Protocol unpack
+    # TCP header table: http://www.networksorcery.com/enp/protocol/tcp.htm
+    # take first 20 char of TCP header
+    #unpack source port (16 bits) , destination port (16 bits), sequence number (32 bits long) and acknowledgement number (32 bits long)
+    # not extracted: Data Offset 	reserved 	ECN 	Control Bits 	Window Checksum 	Urgent Pointer
+def TCP_protocol (header_length,eth_length,pkt):
+    t= header_length+eth_length
+    TCP_Header= struct.unpack('! H H L L B B H H H', pkt[t:t+20]) #TCp length= 20bytes
+    protocol= 'TCP'
+    source_port= TCP_Header[0]
+    dest_port= TCP_Header[1]
+    sequence= TCP_Header[2]
+    acknum= TCP_Header[3]
+    return protocol, source_port,dest_port,sequence,acknum
+
+
+    #ICMP packet unpack
+    #ICMP header table:http://www.networksorcery.com/enp/protocol/icmp.htm
+    #unpack type(8 bits), code(8 bits) and checksum(16 bits)
+def ICMP_packet(header_length,eth_length,pkt):
+    i=header_length+eth_length
+    ICMP_header= struct.unpack('!BBH', pkt[i:i+4]) # ICMP length =4bytes
+
+    protocol = 'ICMP'
+    type= ICMP_header[0]
+    code= ICMP_header[1]
+    checksum= ICMP_header[2]
+    return protocol,type,code,checksum
+
+
+#UDP packet unpack
+#http://www.networksorcery.com/enp/protocol/udp.htm
+#unpack source port (16 bits= H), destination port(16 bits= H), length (16 bits= H), chechsum(16 bits= H)
+def UDP_packet(header_length,eth_length,pkt):
+    u=header_length+eth_length
+    UDP_header= struct.unpack('!HHHH', pkt[u:u+8]) #udp size 8bytes
+
+    protocol='UDP'
+    source_port= UDP_header[0]
+    dest_port= UDP_header[1]
+    length = UDP_header[2]
+    check_sum= UDP_header[3]
+    return protocol,source_port,dest_port,length,check_sum
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -67,11 +171,12 @@ def main():
         except socket.timeout:
             print ' error in receiving data'
 
-        dest_mac, src_mac,protocol,pkt= ethernet_unpack(raw_data)
-        ip_version, header_length, total_length, identification = IP_header_unpack(raw_data)
-        print (' dest_mac: ' +dest_mac + ' source_mac: '+ src_mac+' protocol: '+str(protocol))
+        dest_mac, src_mac,type,pkt= ethernet_unpack(raw_data)
+        ip_version, header_length, total_length, identification,ttl,ip_protocol,source_addr,dest_addr = IP_header_unpack(raw_data)
+        print (' dest_mac: ' +dest_mac + ' source_mac: '+ src_mac+' type: '+str(type))
         print ''
-        print ('ip version: '+str(ip_version)+ ' header length: ' +str(header_length)+ ' total length: '+ str(total_length)+ ' identification: ' + str(identification))
+        print ('ip version: '+str(ip_version)+ ' header length: ' +str(header_length)+ ' total length: '+ str(total_length)+ ' identification: ' + str(identification)
+               + ' TTL: ' +str(ttl)+ ' source ip: ' +str(source_addr)+ ' destination ip: '+ str(dest_addr) + ' ip protocol: '+ str(ip_protocol))
         print''
         print''
 
