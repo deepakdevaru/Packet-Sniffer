6b775cc7c28d62d26666bf3dc06d4568dd945d57
deepakdevaru <joshi.deepak26@gmail.com>
2017-11-26 00:52:36 -0800

    flags added

--- Packet-Sniffer.py
+++ Packet-Sniffer.py
@@ -24,18 +24,37 @@ def mac_address(address_data):
 def IP_header_unpack(pkt):
     unpack_data= struct.unpack('!B B H H H B B H 4s 4s', pkt[:20])
     versionIHL= unpack_data[0] #unpack version and  IP header length
-    version= versionIHL >> 4 # to extract version, bit shit by 4 bits as version occupies 4bits in iptable
+
+    version= versionIHL >> 4 # to extract version, bit shift by 4 bits as version occupies 4bits in iptable
     header_length =  versionIHL & 0xf
 
     # differentiated services not unpacked
 
     total_length=  unpack_data[2]
     identification= unpack_data[3]
+    return version, header_length, total_length, identification
 
 
-    print 'versionIHL ' + str(versionIHL)
-    print 'version `' + str(version)
-    print 'header length '+str(header_length)
+
+#getiing flag details:
+#flags takes up 3 bits in the iptable.
+# R (reserved bit) - 1bit size- values( 0- fragment if necessary, 1- do not fragment)
+#MF (more fragments : 1bit  values( 0- this is last fragment, 1- more fragment follow this fragment)
+def get_flags(pkt):
+    flagR = {o: 'o: Reserved bits'}
+    flagDF = {o: 'o: fragment if necessary', 1: '1: do not fragment'}
+    flagMF = {o: 'o: last fragment', 1:' 1: more fragment to follow'}
+
+# calculate R,DF, MF values:
+    #calculate R value:
+    #0x8000 in binary is 1000000000000000 which is 16 bits long. we need to get value at 15th position in the ipheader as 15th postiton corressponds to Reserved bits.
+    #data till 16 bits are made zero and is then bit-shifted to right to 15th postions
+    R= pkt & 0x8000
+    R >>= 15
+    
+
+
+
 
 
     
@@ -49,10 +68,12 @@ def main():
             print ' error in receiving data'
 
         dest_mac, src_mac,protocol,pkt= ethernet_unpack(raw_data)
-        IP_header_unpack(raw_data)
-        print ' dest_mac ' +dest_mac
-        print 'source_mac '+ src_mac
-        print ' protocol '+str(protocol)
+        ip_version, header_length, total_length, identification = IP_header_unpack(raw_data)
+        print (' dest_mac: ' +dest_mac + ' source_mac: '+ src_mac+' protocol: '+str(protocol))
+        print ''
+        print ('ip version: '+str(ip_version)+ ' header length: ' +str(header_length)+ ' total length: '+ str(total_length)+ ' identification: ' + str(identification))
+        print''
+        print''
 
 
 
